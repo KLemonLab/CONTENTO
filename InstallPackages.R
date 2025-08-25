8d164c43fed2ab0b9a4681cfc62c7b1b2e7da2ef
IsabelFE <isabel.fernandez.escapa@gmail.com>
2025-08-25 18:23:09 +0200

    Code to install packages

---
+++ InstallPackages.R
@@ -0,0 +1,20 @@
+# Install BiocManager if not already present
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+# CRAN packages
+cran_packages <- c(
+  "shiny", "shinydashboard", "shinycssloaders", "DT", "plotly", 
+  "tidyverse", "purrr", "viridis", "RColorBrewer", 
+  "scales", "tools", "grid", "ggiraph"
+)
+
+# Install missing CRAN packages
+install.packages(setdiff(cran_packages, installed.packages()[,"Package"]))
+
+# Bioconductor packages
+bioc_packages <- c("ComplexHeatmap", "DESeq2", "ComplexUpset")
+
+# Install missing Bioconductor packages
+BiocManager::install(setdiff(bioc_packages, installed.packages()[,"Package"]))
