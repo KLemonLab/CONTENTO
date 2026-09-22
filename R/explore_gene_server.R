f67431137a7bf7432466c4b22903063c8f79f036
Jeyasri Venkat <137555229+jey-11@users.noreply.github.com>
2026-09-22 15:20:26 -0500

    Error message for not enough expression for gene

--- R/explore_gene_server.R
+++ R/explore_gene_server.R
@@ -351,6 +351,10 @@ explore_gene_server <- function(input, output, session, state, organism) {
     tryCatch({
       vp_gene <- state$varpart_obj()$varPart[input$geneSelect, ]
       vp_df   <- data.frame(Factor = names(vp_gene), Variance = as.numeric(vp_gene))
+      
+      if (all(vp_df$Variance == 0, na.rm = TRUE)) {
+        validate(need(FALSE, "Not enough variance in selected gene to compute variance partitioning."))
+      }
       ggplot(vp_df, aes(x = reorder(Factor, -Variance), y = Variance)) +
         geom_col(fill = "steelblue") +
         scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
@@ -361,7 +365,6 @@ explore_gene_server <- function(input, output, session, state, organism) {
               panel.grid.minor.x = element_blank())
     }, error = handle_varpart_plot_error)
   })
-  
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ##### Reactive: Neighborhood Analysis #####
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
